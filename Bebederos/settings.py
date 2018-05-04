326000d6a1c66f62671dd90c8c976ae6e7151b99
dxviidmg <dmg_92@hotmail.com>
2018-05-04 12:02:28 -0500

    Revert "v1.8.2 delete get_full_name in files admin.py"
    This reverts commit 79eb8af7399b0d733e53cb1fbc36baa45ee0f21b.
    
    	modificado:    construccion/admin.py
    	modificado:    templates/base.html

--- Bebederos/settings.py
+++ Bebederos/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Mexico_City'
 
