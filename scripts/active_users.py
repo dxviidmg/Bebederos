1c974209de5ad87eaa38943379e6078f05150e3a
dxviidmg <dmg_92@hotmail.com>
2019-11-09 22:35:15 -0600

    1.21.4 activar users inifed 2

--- scripts/active_users.py
+++ scripts/active_users.py
@@ -3,10 +3,26 @@ from accounts.models import Perfil
 from django.db.models import Q
 
 def run():
-	users = User.objects.filter(is_active=True)
+	print('1: Ver usuarios activos')
+	print('2: Ver todos los usuarios')
+	opcion = input('Opcion: ')
+	if opcion == '1':
+		users = User.objects.filter(is_active=True)
+	else:
+		perfiles = Perfil.objects.exclude(tipo='Escuela')
+		users = User.objects.filter(perfil__in=perfiles)
 #	print("USERS NO activos", users_no_activos)
+	users.order_by('username')
 	for user in users:
 		try:
-			print(user, user.username, user.perfil.tipo)
-		except:
-			print(user, user.username, 'No tiene perfil')
+			if opcion == '1':
+				print('Username:' + user.username + ' Nombre:' + user.first_name + ' ' + user.last_name + ' Tipo:' + user.perfil.tipo)
+			else:	
+				status = 'Activo'
+				if user.is_active == False:
+					status = 'Inactivo'
+				print('Username:' + user.username + ' Nombre:' + user.first_name + ' ' + user.last_name + ' Tipo:' + user.perfil.tipo + ' Status: ' + status)
+		except Exception as e:
+#			print(user, user.username, 'No tiene perfil')
+#			print(e)
+			pass
